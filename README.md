59e379b2ffb8b46581001350b647766365f70326
Petar Petković <petar.petkovic@pmf.edu.rs>
2016-10-18 22:51:00 +0200

    ver2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Personal-Portfolio-Webpage
+
+1) write about me
+2) animation on portfolio projects
+3) make contact me to work
---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+	   	e.preventDefault();
+		var target = this.hash,
+	    $target = $(target);
+
+    	$('html, body').stop().animate( {
+      		'scrollTop': $target.offset().top-40
+    		}, 900, 'swing', function () {
+      		window.location.hash = target;
+   		} );
+  	} );
+} );
