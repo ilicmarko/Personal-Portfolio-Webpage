798c716bde1216bc0a76b24a9c7027e9cd56876a
ilic.marko <ilic.marko.05@live.com>
2016-10-23 21:22:10 +0200

    Trebalo bi da je sredjeno

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,13 @@
 jQuery(document).ready(function($) {
-	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
-	   	e.preventDefault();
-		var target = this.hash,
-	    $target = $(target);
-    	$('html, body').stop().animate( {
-      		'scrollTop': $target.offset().top
-    		}, 900, 'swing', function () {
-      		window.location.hash = target;
-   		} );
-  	} );
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+      e.preventDefault();
+    var target = $(this.hash);
+      $('html, body').stop().animate( {
+          'scrollTop': target.offset().top - 40
+        }, 900, 'swing', function () {
+          window.location.hash = target;
+      } );
+    } );
 } );
 
 $(function(){
